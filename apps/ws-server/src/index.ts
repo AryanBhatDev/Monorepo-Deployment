3051e7e96be8a20da11c8dc5912762faf9cec176
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-10 23:18:29 +0530

    completed the app v1

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -4,11 +4,16 @@ import { client } from '@repo/database/client'
 const wss = new WebSocketServer({port:8080});
 
 wss.on('connection',async (s)=>{
-    await client.user.create({
-        data:{
-            username:Math.random().toString(),
-            password:Math.random().toString(),
-        }
-    })
-    s.send('Hi there, you are connected to websocket')
+    try{
+        const res = await client.user.create({
+            data:{
+                username:Math.random().toString(),
+                password:Math.random().toString(),
+            }
+        })
+        console.log(res)
+        s.send('Hi there, you are connected to websocket')
+    }catch(e){
+        console.error(e);
+    }
 })
